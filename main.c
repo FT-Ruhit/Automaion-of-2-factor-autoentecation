6f2ebd8a95bb3bb3ea11b23b1b788a4c280ef54f
Ruhit <ruhit2588@gmail.com>
2021-07-29 21:40:59 +0600

    First commit

---
+++ main.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+#include <direct.h>
+
+void cptoclipboard(char *str)
+{
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, strlen(str) + 1);
+    memcpy(GlobalLock(hMem), str, strlen(str) + 1);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+
+int main()
+{
+    char str1[12], str2[12], str3[12], str4[12], str5[12], str6[12], str7[12], str8[12], str9[12], str10[12], str11[12], str12[12], str13[12], str14[12], str15[12], str16[12], temp[3];
+    FILE *rf, *nf;
+    int num;
+    rf = fopen("github-recovery-codes.txt", "r");
+    if (rf == NULL)
+    {
+        printf("File not found!\n");
+        return 0;
+    }
+    fscanf(rf, "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11, str12, str13, str14, str15, str16);
+    fclose(rf);
+    // printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11, str12, str13, str14, str15, str16);
+    nf = fopen("temp.bin", "rb");
+    if (nf == NULL)
+    {
+        nf = fopen("temp.bin", "wb");
+        num = 1;
+    }
+    else
+    {
+        fgets(temp, 3, nf);
+        num = atoi(temp);
+    }
+    fclose(nf);
+    switch (num)
+    {
+    case 1:
+        printf("%s", str1);
+        cptoclipboard(str1);
+        break;
+    case 2:
+        printf("%s", str2);
+        cptoclipboard(str2);
+        break;
+    case 3:
+        printf("%s", str3);
+        cptoclipboard(str3);
+        break;
+    case 4:
+        printf("%s", str4);
+        cptoclipboard(str4);
+        break;
+    case 5:
+        printf("%s", str5);
+        cptoclipboard(str5);
+        break;
+    case 6:
+        printf("%s", str6);
+        cptoclipboard(str6);
+        break;
+    case 7:
+        printf("%s", str7);
+        cptoclipboard(str7);
+        break;
+    case 8:
+        printf("%s", str8);
+        cptoclipboard(str8);
+        break;
+    case 9:
+        printf("%s", str9);
+        cptoclipboard(str9);
+        break;
+    case 10:
+        printf("%s", str10);
+        cptoclipboard(str10);
+        break;
+    case 11:
+        printf("%s", str11);
+        cptoclipboard(str11);
+        break;
+    case 12:
+        printf("%s", str12);
+        cptoclipboard(str12);
+        break;
+    case 13:
+        printf("%s", str13);
+        cptoclipboard(str13);
+        break;
+    case 14:
+        printf("%s", str14);
+        cptoclipboard(str14);
+        break;
+    case 15:
+        printf("%s", str15);
+        cptoclipboard(str15);
+        break;
+    default:
+        break;
+    }
+    num++;
+    if (num == 16)
+    {
+        printf("%s\n\n", str16);
+        cptoclipboard(str16);
+        printf("The list is end\n");
+        num = 1;
+    }
+    nf = fopen("temp.bin", "wb");
+    fprintf(nf, "%d", num);
+    fclose(nf);
+    return 0;
+}
